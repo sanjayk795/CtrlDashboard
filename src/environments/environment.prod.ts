a52e9c5aa4652e2ca3192ba7ba99ae05aecbd376
sharadxebia <s.sarawswat@xebia.com>
2017-11-06 23:08:36 +0530

    ProdEnv

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,10 @@
 export const environment = {
-    production: true,
-    api_url: 'https://conduit.productionready.io/api',
-    off_report_path: '/reports/off-report',
-    login_path: '/users/login',
-    login_auth_path: '/users/user'
+    "production": false,
+    "api_url": '',
+    "/report-availability/off-price": 'http://localhost:8000/assets/mock/report-availability/off-price.json',
+    "/report-availability/full-price": 'http://localhost:8000/assets/mock/report-availability/full-price.json',
+    "/report-availability/supply-chain": 'http://localhost:8000/assets/mock/report-availability/supply-chain.json',
+    "/report-availability/sales": 'http://localhost:8000/assets/mock/report-availability/sales.json',
+    "login_path": 'http://localhost:8000/assets/mock/users/login.json',
+    "login_auth_path": 'http://localhost:8000/assets/mock/users/user.json'
 };
