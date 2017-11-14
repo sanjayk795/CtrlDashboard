877f5d114a037da9e4ecae7e13d8417072e5bf1d
sharadxebia <s.sarawswat@xebia.com>
2017-11-14 12:55:56 +0530

    LiveUrlAdded

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,6 @@ export const environment = {
     "/report-availability/full-price": 'http://172.27.25.64:8080/api/reportsapi/200/',
     "/report-availability/supply-chain": 'http://localhost:8000/assets/mock/report-availability/supply-chain.json',
     "/report-availability/sales": 'http://172.27.25.64:8080/api/reportsapi/300/',
-    "login_path": 'http://localhost:8000/assets/mock/users/login.json',
+    "login_path": 'http://172.27.25.64:8080/api/validateapplicationuser/admin/admin',
     "login_auth_path": 'http://localhost:8000/assets/mock/users/user.json'
 };
